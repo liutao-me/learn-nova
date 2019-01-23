6fa3c26235dcb5e8086ec22c8447570eb929026b
Rennes <leienshu@126.com>
2019-01-23 15:51:56 +0800

    创建数据表和用户模型

---
+++ database/migrations/2019_01_23_073617_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->comment('用户ID');
+            $table->integer('visitor_id')->comment('访客ID');
+            $table->text('priority')->comment('优先级');
+            $table->text('title')->comment('标题');
+            $table->text('body')->comment('内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
