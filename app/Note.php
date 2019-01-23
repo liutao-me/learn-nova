04b42b361449fbbac24a04eb96dc337d25b90c7e
Rennes <leienshu@126.com>
2019-01-23 16:17:07 +0800

    定义模型关系

--- app/Note.php
+++ app/Note.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    // 定义优先级
+    const LOW_PRIORITY = 'Low';
+    const MEDIUM_PRIORITY = 'Medium';
+    const HIGH_PRIORITY = 'High';
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function visitor()
+    {
+        return $this->belongsTo('App\visitor');
+    }
+
+    // 获取优先级
+    public static function getPriority()
+    {
+        return [
+            self::LOW_PRIORITY => self::LOW_PRIORITY,
+            self::MEDIUM_PRIORITY => self::MEDIUM_PRIORITY,
+            self::HIGH_PRIORITY => self::HIGH_PRIORITY,
+        ];
+    }
 }
--- app/Visitor.php
+++ app/Visitor.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
-    //
+    // 定义用户常量
+    const ORGANIC_TYPE = 'Organic';  //基本类型
+    const USER_SUBMITTED_TYPE = 'User Submitted';  //用户提交类型
+
+    const PROSPECT_STATUS = 'Prospect';  //预期
+    const VISITOR_STATUS = 'Visitor';  //访客状态
+    const CUSTOMER_STATUS = 'Customer'; //客户状态
+
+    protected $fillable = [
+        'type',
+        'status',
+        'email',
+        'name'
+    ];
+
+    public function notes()
+    {
+        return $this->hasMany('App\Note');
+    }
+
+    // 获取类型
+    public static function getTypes()
+    {
+        return [
+            self::ORGANIC_TYPE => self::ORGANIC_TYPE,
+            self::USER_SUBMITTED_TYPE => self::USER_SUBMITTED_TYPE,
+        ];
+    }
+
+    //获取状态
+    public static function getStatuses()
+    {
+        return [
+            self::PROSPECT_STATUS => self::PROSPECT_STATUS,
+            self::VISITOR_STATUS => self::VISITOR_STATUS,
+            self::CUSTOMER_STATUS => self::CUSTOMER_STATUS,
+        ];
+    }
 }
